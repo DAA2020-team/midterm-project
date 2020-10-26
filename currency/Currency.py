f69d4de7432b50337024afb2d100189326533e9f
giorge1 <g.ammendola4@studenti.unisa.it>
2020-10-26 13:01:07 +0100

    min_denomination
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

--- currency/Currency.py
+++ currency/Currency.py
@@ -22,4 +22,24 @@ class Currency:
     def del_denomination(self, value):
         del self._denominations[value]
 
+    def min_denomination(self, value=None):
+        if self._denominations.is_empty():
+            raise ValueError("No denomination present")
+        if value is None:
+            return self._denominations.find_min()
+        v = self._denominations.find_ge(value)
+        if v is None:
+            raise ValueError("No denomination greater than " + str(value) + "present")
+        return v
+
+
+cur = Currency("EUR")
+cur.add_denomination(1)
+cur.add_denomination(3)
+cur.add_denomination(5)
+cur.add_denomination(7)
+
+cur.del_denomination(3)
+
+print(cur.min_denomination(0))
 
