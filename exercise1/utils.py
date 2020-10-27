426c1501f199e77fa0eb3e979f4e0cdb6780c281
VincenzoPetrone <51163843+v8p1197@users.noreply.github.com>
2020-10-27 12:29:53 +0100

    segmented_sieve() method deleted. primes_in_range() has been implemented instead

--- exercise1/utils.py
+++ exercise1/utils.py
@@ -1,32 +1,48 @@
-from math import sqrt
+from math import floor, sqrt
 
 
 def bitify(s):
     return ''.join(format(c, 'b') for c in bytearray(s, 'utf-8'))
 
 
-def segmented_sieve(start: int, stop: int):
-    """Returns a list containing all primes between start and stop included.
+def simple_sieve(limit, primes):
+    """Finds all primes smaller than limit using simple sieve of eratosthenes. It stores found primes in list prime."""
+    mark = [False] * (limit + 1)
+
+    for i in range(2, limit + 1):
+        if not mark[i]:
+
+            # If not marked yet,
+            # then its a prime
+            primes.append(i)
+            for j in range(i, limit + 1, i):
+                mark[j] = True
+
+def primes_in_range(low, high):
+    """
+    Returns a list containing all primes between start and stop included.
     It is an implementation of Sieve of Eratosthenes algorithm:
     https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html"
 
-    Let R be stop and L be start.
-    Time complexity is O((R−L+1) * log(log(R)) + sqrt(R) * log(log(sqrt(R))))"""
-    lim = int(sqrt(stop))
-    mark = [False] * (lim + 1)
-    primes = []
+    Let R be high and L be low.
+    Time complexity is O((R−L+1) * log(log(R)) + sqrt(R) * log(log(sqrt(R))))
+    """
+    limit = floor(sqrt(high)) + 1
+    primes = list()
+    simple_sieve(limit, primes)
 
-    for i in range(2, lim + 1):
-        if not mark[i]:
-            primes.append(i)
-            for j in range(i * i, lim + 1, i):
-                mark[j] = True
+    n = high - low + 1
 
-    is_prime = [True] * (stop - start + 1)
-    for i in primes:
-        for j in range(max(i * i, (start + i - 1) // (i * i)), stop + 1, i):
-            is_prime[j - start] = False
-    if start == 1:
-        is_prime[0] = False
+    mark = [False] * (n + 1)
 
-    return is_prime
+    for i in range(len(primes)):
+        loLim = floor(low / primes[i]) * primes[i]
+        if loLim < low:
+            loLim += primes[i]
+        if loLim == primes[i]:
+            loLim += primes[i]
+        for j in range(loLim, high + 1, primes[i]):
+            if j != primes[i]:
+                mark[j - low] = True
+
+    return [i for i in range(low, high + 1) if not mark[i - low]]
