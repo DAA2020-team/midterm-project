12026716f70b5488e2a844a312ad2069ea6d50ba
VincenzoPetrone <51163843+v8p1197@users.noreply.github.com>
2020-10-27 09:54:48 +0100

    Added documentation for double_hashing_hash_map.py

--- data_structures/double_hashing_hash_map.py
+++ data_structures/double_hashing_hash_map.py
@@ -4,30 +4,71 @@ from exercise1.utils import bitify
 
 class DoubleHashingHashMap(HashMapBase):
 
-    __slots__ = '_q'
+    __slots__ = '_z', '_q'
 
-    def __init__(self, cap=17, p=109345121, q=13):
+    # --------------- CONSTRUCTOR ---------------
+
+    def __init__(self, cap=17, z=92821, p=109345121, q=13):
+        """
+        Constructor.
+        :param cap: Initial capacity of the HashMap
+        :param z: Positive prime used for string hashing (default 92821)
+        :param p: Positive prime used for MAD (default 109345121)
+        :param q: Positive prime used for secondary hash function compression (default 13)
+        """
         super().__init__(cap=cap, p=p)
-        self._q = 13
+        self._z = z
+        self._q = q
+
+    # --------------- UTILITY METHODS ---------------
 
     def _hash_code(self, x):
-        return sum(int(c) * self._prime**k for k, c in enumerate(bitify(x)))
+        """
+        Returns the polynomial hash code for string x
+        :param x: string to hash
+        :return: hash code of x
+        """
+        return sum(int(c) * self._z**k for k, c in enumerate(bitify(x)))
 
     def _h(self, x):
-        return (self._hash_code(x) * self._scale + self._shift) % self._prime % len(self._table)
+        """
+        Primary hash function. Implements MAD compression
+        :param x: key to hash
+        :return: compressed primary hash code of x in [0, N - 1]
+        """
+        return (self._hash_code(x) * self._scale + self._shift) % self._prime % self.capacity()
 
     def _d(self, x):
+        """
+        Secondary hash function.
+        :param x: key to hash
+        :return: compressed secondary hash code of x in [1, q - 1]
+        """
         return self._q - self._hash_code(x) % self._q
+
+    # --------------- PUBLIC METHODS ---------------
 
     def __getitem__(self, k):
         pass
 
     def __setitem__(self, k, v):
         if self._n > len(self._table) // 2:  # keep load factor <= 0.5
-            self._resize(2 * len(self._table) - 1)  # number 2^x - 1 is often prime
+            # resize must be called at the biggest prime in segmented_sieve(len(self._table), 2 * len(self._table) - 1)
+            self._resize(2 * self.capacity() - 1)  # number 2^x - 1 is often prime
 
     def __delitem__(self, k):
         pass
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def capacity(self):
+        return len(self._table)
+
+    # --------------- PRIVATE METHODS ---------------
 
     def _bucket_getitem(self, j, k):
         pass
@@ -36,7 +77,4 @@ class DoubleHashingHashMap(HashMapBase):
         pass
 
     def _bucket_delitem(self, j, k):
-        pass
-
-    def __iter__(self):
         pass
