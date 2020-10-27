ac0548452ab09ede6368855d7326682e7e7731aa
VincenzoPetrone <51163843+v8p1197@users.noreply.github.com>
2020-10-27 13:06:55 +0100

    delitem method implemented
    (cherry picked from commit a1ca4807d0b3b12917f812dbafd71822cd697021)
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

--- data_structures/double_hashing_hash_map.py
+++ data_structures/double_hashing_hash_map.py
@@ -1,42 +1,140 @@
 from .hash_map_base import HashMapBase
-from exercise1.utils import bitify
+from .map_base import MapBase
+
+from exercise1.utils import bitify, primes_in_range
 
 
 class DoubleHashingHashMap(HashMapBase):
 
-    __slots__ = '_q'
+    __slots__ = '_z', '_q', '_collision_counter'
 
-    def __init__(self, cap=17, p=109345121, q=13):
+    _AVAIL = object()  # sentinal marks locations of previous deletions
+
+    # --------------- NESTED _Item CLASS ---------------
+
+    class _Item(MapBase._Item):
+
+        def __str__(self):
+            return "{%s: %s}" % (self._key, self._value)
+
+    # --------------- CONSTRUCTOR ---------------
+
+    def __init__(self, cap=17, z=92821, p=109345121, q=13):
+        """
+        Constructor.
+        :param cap: Initial capacity of the HashMap
+        :param z: Positive prime used for string hashing (default 92821)
+        :param p: Positive prime used for MAD (default 109345121)
+        :param q: Positive prime used for secondary hash function compression (default 13)
+        """
         super().__init__(cap=cap, p=p)
-        self._q = 13
+        self._z = z
+        self._q = q
+        self._collision_counter = 0
+
+    # --------------- UTILITY METHODS ---------------
 
     def _hash_code(self, x):
-        return sum(int(c) * self._prime**k for k, c in enumerate(bitify(x)))
+        """
+        Returns the polynomial hash code for string x
+        :param x: string to hash
+        :return: hash code of x
+        """
+        return sum(int(c) * self._z**k for k, c in enumerate(bitify(x)))
 
     def _h(self, x):
-        return (self._hash_code(x) * self._scale + self._shift) % self._prime % len(self._table)
+        """
+        Primary hash function. Implements MAD compression
+        :param x: key to hash
+        :return: compressed primary hash code of x in [0, N - 1]
+        """
+        return (self._hash_code(x) * self._scale + self._shift) % self._prime % self.capacity()
 
     def _d(self, x):
+        """
+        Secondary hash function.
+        :param x: key to hash
+        :return: compressed secondary hash code of x in [1, q - 1]
+        """
         return self._q - self._hash_code(x) % self._q
 
+    # --------------- PUBLIC METHODS ---------------
+
     def __getitem__(self, k):
-        pass
+        j = self._h(k)
+        return self._bucket_getitem(j, k)  # may raise KeyError
 
     def __setitem__(self, k, v):
-        if self._n > len(self._table) // 2:  # keep load factor <= 0.5
-            self._resize(2 * len(self._table) - 1)  # number 2^x - 1 is often prime
+        j = self._h(k)
+        self._bucket_setitem(j, k, v)  # subroutine maintains self._n
+        if self._n > self.capacity() // 2:  # keep load factor <= 0.5
+            self._resize(primes_in_range(self.capacity(), 2 * self.capacity() - 1)[-1])
 
     def __delitem__(self, k):
-        pass
-
-    def _bucket_getitem(self, j, k):
-        pass
-
-    def _bucket_setitem(self, j, k, v):
-        pass
-
-    def _bucket_delitem(self, j, k):
-        pass
+        j = self._h(k)
+        self._bucket_delitem(j, k)  # may raise KeyError
+        self._n -= 1
 
     def __iter__(self):
-        pass
+        for j in range(len(self._table)):  # scan entire table
+            if not self._is_available(j):
+                yield self._table[j]._key
+
+    def items(self):
+        for j in range(len(self._table)):  # scan entire table
+            if not self._is_available(j):
+                yield self._table[j]._key, self._table[j]._value
+
+    def __str__(self):
+        return str([str(item) for item in self._table if item is not None])
+
+    def capacity(self):
+        return len(self._table)
+
+    def get_collisions(self):
+        return self._collision_counter
+
+    # --------------- PRIVATE METHODS ---------------
+
+    def _is_available(self, j):
+        """Return True if index j is available in table."""
+        return self._table[j] is None or self._table[j] is DoubleHashingHashMap._AVAIL
+
+    def _find_slot(self, j, k):
+        """Search for key k in bucket at index j.
+
+        Return (success, index) tuple, described as follows:
+        If match was found, success is True and index denotes its location.
+        If no match found, success is False and index denotes first available slot.
+        """
+        firstAvail = None
+        while True:
+            if self._is_available(j):
+                if firstAvail is None:
+                    firstAvail = j  # mark this as first avail
+                if self._table[j] is None:
+                    return False, firstAvail  # search has failed
+            elif k == self._table[j]._key:
+                return True, j  # found a match
+            j = (j + self._d(k)) % self.capacity()  # keep looking (cyclically)
+            self._collision_counter += 1  # collision found. increment counter
+
+    def _bucket_getitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k))  # no match found
+        return self._table[s]._value
+
+    def _bucket_setitem(self, j, k, v):
+        found, s = self._find_slot(j, k)
+        if not found:
+            self._table[s] = self._Item(k, v)  # insert new item
+            self._n += 1  # size has increased
+        else:
+            self._table[s]._value = v  # overwrite existing
+
+    def _bucket_delitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k))  # no match found
+        self._table[s] = DoubleHashingHashMap._AVAIL  # mark as vacated
